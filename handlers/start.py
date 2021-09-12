14509c052020e1f986c53005dea8fdd4fd6de4a8
Harrybot021 <82758678+Harrybot021@users.noreply.github.com>
2021-09-12 17:05:19 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -101,7 +101,7 @@ async def help(client: Client, message: Message):
 
 **Please press the button below to read the explanation and see the list of available commands !**
 
-⚡ __Powered by {BOT_NAME} RR""",
+⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
